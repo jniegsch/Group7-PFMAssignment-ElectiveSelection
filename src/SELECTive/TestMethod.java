e59f8af5774a0d308df662737588a1a89aadf13a
Jan Niegsch <niegsch.j@gmail.com>
2019-02-16 01:11:35 +0100

    TestMethod
    TestMethod:
    > added the private properties
    > added an invalidator to the types

--- src/SELECTive/TestMethod.java
+++ src/SELECTive/TestMethod.java
@@ -10,10 +10,13 @@ public class TestMethod {
         GROUP_PROJECT,
         INDIVIDUAL_PROJECT,
         PRESENTATION,
-        CASE_PRESENTATION
+        CASE_PRESENTATION,
+        YtestYOLOjustGoForIt_BREAKSHITTHENFIXIT //default option, definitely - and clearly - invalid
     }
     //endregion
 
     //region Private Properties
+    private TestType type = TestType.YtestYOLOjustGoForIt_BREAKSHITTHENFIXIT;
+    private double gradeWeight = 0.0;
     //endregion
 }
