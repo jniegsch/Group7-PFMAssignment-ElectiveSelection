97fb4b63ce9c65ef6f5c150c77810c269ebe6a4a
Tanislas <48248187+Tanislas@users.noreply.github.com>
2019-03-07 10:42:10 +0100

    Fixed saveElective error
    Changed "this.block.toString()" into "Integer.toString(this.block)" since toString cannot be used for integer primitives

--- src/SELECTive/Elective.java
+++ src/SELECTive/Elective.java
@@ -250,7 +250,7 @@ public class Elective {
                 this.program.toString(),
                 keywordString(),
                 this.lectureDay.toString(),
-                this.block.toString(),
+		Integer.toString(this.block),
                 Long.toString(this.lecturerId),
         };
 
